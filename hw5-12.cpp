5149136b5171989a396af8deb0cfaf8116b5ff5f
Jose Marti <josemarti@my.tridenttech.edu>
2024-07-23 20:14:23 -0400

    Update hw5-12.cpp

--- hw5-12.cpp
+++ hw5-12.cpp
@@ -7,6 +7,24 @@ using namespace std;
 
 int main()
 {
+double discountRate = 0.5;
+  double priceItem1 = 0.0;
+  double priceItem2 = 0.0;
+  double amountTotal = 0.0;
 
+  cout << "Enter price of item 1: ";
+  cin >> priceItem1;
+
+  cout << "Enter price of item 2: ";
+  cin >> priceItem2;
+
+  if (priceItem1 < priceItem2)
+    priceItem1 -= priceItem1 * discountRate;
+  else
+    priceItem2 -= priceItem2 * discountRate;
+
+  cout << fixed << setprecision(2);
+  amountTotal = priceItem1 + priceItem2;
+  cout << amountTotal;
 	return 0;
 }	//end of main function
